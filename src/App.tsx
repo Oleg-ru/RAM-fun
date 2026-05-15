f29358ad29d21c36569ae84ff30f7534dd4fb0ab
oleg <oleg_09-95@mail.ru>
2026-05-15 22:15:52 +0300

    R&M-fun Опять правлен баг загрузки

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import type {Search} from "./types/search.ts";
 
 function App() {
 
-    const [searchParams, setSearchParams] = useState()
+    const [searchParams, setSearchParams] = useState({})
 
     const onSearch: Search = (searchName: string, searchStatus?: string) => {
         setSearchParams({searchName, searchStatus});
